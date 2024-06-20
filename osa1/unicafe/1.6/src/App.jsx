705eff964254bf00193cd9133ce4f42461a8cccb
JuusoM123 <168100819+JuusoM123@users.noreply.github.com>
2024-06-20 18:53:20 +0300

    Add files via upload

---
+++ osa1/unicafe/1.6/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = (props) => {
+  return(
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good'/>
+      <Button handleClick={handleNeutralClick} text='neutral'/>
+      <Button handleClick={handleBadClick} text='bad'/>
+
+      <h1>statistics</h1>
+      <Display good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
